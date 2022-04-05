d8eb13807f07a110aa3a8c618d0baa2c862cfa37
Cory Thompson <cory.thompson98@gmail.com>
2022-04-05 13:58:39 -0500

    final additions to the page template

--- src/page-template.js
+++ src/page-template.js
@@ -23,7 +23,7 @@ const generateEngineers = engineersArr => {
                 return `
                 <div class="card cardB" style="width: 20rem;">
                 <h4 class="card-header">${name}</h4>
-                <h5 class="card-header">👓${role}</h5>  
+                <h5 class="card-header">${role}</h5>  
                     <div class="card-body">
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">ID: ${id}</li>
@@ -37,10 +37,59 @@ const generateEngineers = engineersArr => {
         .join('')}
     `};
 
-const generateInters = internsArr => {
-
-};
+const generateInterns = internsArr => {
+    return `
+    ${internsArr
+        .map(({ name, id, email, school, role }) => {
+            return `
+            <div class="card cardC" style="width: 20rem;">
+            <h4 class="card-header">${name}</h4> 
+            <h5 class="card-header">${role}</h5>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                        <li class="list-group-item">School: ${school}</li>
+                    </ul>
+                </div>
+            </div>
+            `;
+        })
+    .join('')}
+    `};
 
 module.exports = templateData => {
+    const { interns, engineers, ...manager } = templateData;
 
+    return `
+    <!DOCUTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Team Profile Generator</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <div class="container flex-row justify-space-between align-center py-3">
+                    <h1 class="page-title text-secondary bg-dark py-2 px-3">My Team</h1>
+                </div>
+            </nav>
+        </header>
+        <main class="container my-5">
+            ${generateManager(manager)}
+            ${generateEngineers(engineers)}
+            ${generateInterns(interns)}
+        </main>
+    </body>
+    </html>
+    `;
 };
